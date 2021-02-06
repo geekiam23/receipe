367b1768c0b09f28cdb33422616c47de30bec9d0
Will Larry <will23larry@gmail.com>
2021-02-06 17:12:19 -0500

    Setup Prettier and Eslint

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-const tailwindcss = require('tailwindcss');
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const tailwindcss = require("tailwindcss");
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
+  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
 };
