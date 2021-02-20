ea3c0259eda94161a9b3a8fc362a6564c8cb08a7
Will Larry <will23larry@gmail.com>
2021-02-20 10:07:24 -0500

    Add ability to search for recipe

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,29 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = (): ReactElement => {
   const [openMenu, setOpenMenu] = useState(false);
+  const [searchQuery, setSearchQuery] = useState(null);
+  const [searchData, setSearchData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&apiKey=${process.env.REACT_APP_SPOONTACULAR_API_KEY_2}`
+    )
+      .then((res) => res.json())
+      .then((data) => setSearchData(data.results));
+  }, [searchQuery]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery(e.target.value);
+  };
+
+  const handleKeypress = (e) => {
+    if (e.keyCode === 13) {
+      handleSearch(e);
+    }
+  };
 
   return (
     <nav className="bg-white shadow">
@@ -28,7 +49,7 @@ const Navbar = (): ReactElement => {
             </div>
           </div>
           <div className="flex-1 flex items-center justify-center px-2 lg:ml-6 lg:justify-end">
-            <div className="max-w-lg w-full lg:max-w-xs">
+            <div className="relative w-full bg-white max-w-lg z-10">
               <label htmlFor="search" className="sr-only">
                 Search
               </label>
@@ -54,8 +75,30 @@ const Navbar = (): ReactElement => {
                   className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   placeholder="Search"
                   type="search"
+                  value={searchQuery}
+                  onChange={handleSearch}
+                  onKeyPress={handleKeypress}
                 />
               </div>
+              <ul className="divide-y divide-gray-200 bg-white border border-gray-100 w-full absolute ">
+                {searchData &&
+                  searchData.map((recipe) => (
+                    <Link to={`/recipe/${recipe.id}`} key={recipe.id}>
+                      <li className="p-4 flex">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={recipe.image}
+                          alt=""
+                        />
+                        <div className="ml-3">
+                          <p className="text-sm font-medium text-gray-900">
+                            {recipe.title}
+                          </p>
+                        </div>
+                      </li>
+                    </Link>
+                  ))}
+              </ul>
             </div>
           </div>
           <div className="flex items-center lg:hidden">
