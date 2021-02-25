cdf266b624c83c345d3c88a2fb2e7670dc227057
Will Larry <will23larry@gmail.com>
2021-02-25 16:27:39 -0500

    Add circleMinus, CircleCheck, & Star icons

---
+++ src/components/icons/circle-minus.tsx
@@ -0,0 +1,21 @@
+const CircleMinus = ({ size, fill = "none" }) => {
+  return (
+    <div className={size}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill={fill}
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default CircleMinus;
